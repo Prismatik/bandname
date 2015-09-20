6d677058a9a227e85ddf79921858bcabdbbbd4c0
Drew Showalter <maelstroswim@gmail.com>
2015-09-21 02:09:18 +1000

    added choices

--- bandname.js
+++ bandname.js
@@ -1,10 +1,15 @@
 var adjectives = require('./adjectives.json');
-var animals = require('./animals.json');
 
 var array_member = function(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 };
 
-module.exports = function() {
-  return (array_member(adjectives)) + " " + (array_member(animals));
+module.exports = function(genre) {
+  var names = ['animals', 'asoiaf', 'lotr'];
+
+  //where be muh es6?
+  (genre && names.some(function(elem) {if (elem == genre); return elem})) ? genre = genre : genre = 'animals';
+  var noun = require('./' + genre + '.json');
+
+  return (array_member(adjectives)) + " " + (array_member(noun));
 };
