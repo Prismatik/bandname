7a8fa398d77f6e8e12807f2f7cdae6e6c3d8223e
David Banham <david@banham.id.au>
2015-09-21 11:52:22 +1000

    Allow pluggable adjective types

--- bandname.js
+++ bandname.js
@@ -1,21 +1,25 @@
 var path = require('path');
 var fs = require('fs');
 
-var adjectives = require('./adjectives/adjectives.json');
-
 const DEFAULT_NOUN = 'animals';
+const DEFAULT_ADJECTIVE = 'adjectives';
 
-var noun_types = fs.readdirSync('./nouns').map(function(filename) {
+var just_the_name = function(filename) {
   return path.parse(filename).name;
-});
+};
+
+var adjective_types = fs.readdirSync('./adjectives').map(just_the_name);
+var noun_types = fs.readdirSync('./nouns').map(just_the_name);
 
 var array_member = function(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 };
 
-module.exports = function(noun_type) {
+module.exports = function(noun_type, adjective_type) {
   if (!noun_type) noun_type = DEFAULT_NOUN;
+  if (!adjective_type) adjective_type = DEFAULT_ADJECTIVE;
   var nouns = require('./nouns/' + noun_type + '.json');
+  var adjectives = require('./adjectives/' + adjective_type + '.json');
 
   return (array_member(adjectives)) + " " + (array_member(nouns));
 };
